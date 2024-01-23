7c2b2e6e53f9fe9e97be895bff393c8847dd54af
jinpulol <jesselehmonen@gmail.com>
2024-01-23 19:26:30 +0200

    Assigment done, calculator with + and - operators

--- laskin/App.js
+++ laskin/App.js
@@ -1,10 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [firstOperand, setFirstOperand] = useState("");
+  const [secondOperand, setSecondOperand] = useState("");
+  const [operator, setOperator] = useState("");
+
+  const result = () => {
+    const num1 = parseFloat(firstOperand);
+    const num2 = parseFloat(secondOperand);
+
+    if (operator == "+") {
+      return num1 + num2;
+    } else if (operator == "-") {
+      return num1 - num2;
+    }
+  }
+
   return (
+
     <View style={styles.container}>
-      <Text>PLACEHOLDER TEXT</Text>
+      <View>
+        <Text style={{fontSize:20}}>Result: {result()}</Text>
+      </View>
+      <View>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          onChangeText={input => setFirstOperand(input)} />
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          onChangeText={input => setSecondOperand(input)} />
+      </View>
+      <View style={styles.button}>
+        <Button title="+" onPress={_ => setOperator("+")} />
+        <Button title="-" onPress={_ => setOperator("-")} />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +52,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    textAlign: 'center',
+    fontSize: 18,
+    height: 40,
+    width: 160,
+    margin: 5,
+    borderWidth: 1,
+  },
+  button: {
+    flexDirection: 'row',
+    columnGap: 15,
+  }
 });
